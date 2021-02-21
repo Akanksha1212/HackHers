2e5a8b5da9cdce4de4263d5bbc7fda93adb58bd0
Akanksha1212 <akankshasingh121297@gmail.com>
2021-02-21 19:34:11 +0530

    final commit

---
+++ android/app/src/main/kotlin/com/example/spacesafari/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spacesafari
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:spacesafari/panarama.dart';
+
+void main() => runApp(MaterialApp(home: MyApp()));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final samples = [
+      Sample(
+        'Earth',
+        'Sphere with an image texture and rotation animation.',
+        Icons.language,
+        () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => PanoramaPage())),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ARKit Demo'),
+      ),
+      body:
+          ListView(children: samples.map((s) => SampleItem(item: s)).toList()),
+    );
+  }
+}
+
+class SampleItem extends StatelessWidget {
+  const SampleItem({Key key, this.item}) : super(key: key);
+  final Sample item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () => item.onTap(),
+        child: ListTile(
+          leading: Icon(item.icon),
+          title: Text(
+            item.title,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          subtitle: Text(
+            item.description,
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Sample {
+  const Sample(this.title, this.description, this.icon, this.onTap);
+  final String title;
+  final String description;
+  final IconData icon;
+  final Function onTap;
+}
